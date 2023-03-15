853fe585c5deeed76dfc34950071ffd974081bc0
Barengific <75044077+Barengific@users.noreply.github.com>
2023-03-15 23:38:56 +0000

    Initial

---
+++ Assets/Scripts/LevelGeneration.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGeneration : MonoBehaviour
+{
+    public GameObject Tile1;
+    public GameObject Tile2;
+    public GameObject StartTile;
+
+    private float Index = 0;
+
+    private void Start()
+    {
+        GameObject StartPlane1 = Instantiate(StartTile, transform);
+        StartPlane1.transform.position = new Vector3(0, 7, 0);
+        GameObject StartPlane2 = Instantiate(StartTile, transform);
+        StartPlane2.transform.position = new Vector3(0, -1, 0);
+        GameObject StartPlane3 = Instantiate(StartTile, transform);
+        StartPlane3.transform.position = new Vector3(0, -9, 0);
+        GameObject StartPlane4 = Instantiate(StartTile, transform);
+        StartPlane4.transform.position = new Vector3(0, -17, 0);
+        GameObject StartPlane5 = Instantiate(StartTile, transform);
+        StartPlane5.transform.position = new Vector3(0, -25, 0);
+    }
+
+    private void Update()
+    {
+        gameObject.transform.position += new Vector3(0, 4 * Time.deltaTime, 0);
+
+        if (transform.position.y >= Index)
+        {
+            int RandomInt1 = Random.Range(0, 2);
+
+            if (RandomInt1 == 1)
+            {
+                GameObject TempTile1 = Instantiate(Tile1, transform);
+                TempTile1.transform.position = new Vector3(0, -16, 0);
+            }
+            else if (RandomInt1 == 0)
+            {
+                GameObject TempTile1 = Instantiate(Tile2, transform);
+                TempTile1.transform.position = new Vector3(0, -16, 0);
+            }
+
+            int RandomInt2 = Random.Range(0, 2);
+
+            if (RandomInt2 == 1)
+            {
+                GameObject TempTile2 = Instantiate(Tile1, transform);
+                TempTile2.transform.position = new Vector3(0, -24, 0);
+            }
+            else if (RandomInt2 == 0)
+            {
+                GameObject TempTile2 = Instantiate(Tile2, transform);
+                TempTile2.transform.position = new Vector3(0, -24, 0);
+            }
+
+            Index = Index + 15.95f;
+        }
+    }
+}
