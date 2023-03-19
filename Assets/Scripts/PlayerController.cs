409cd06620894af0341d93d40024945e6b62bbba
Barengific <75044077+Barengific@users.noreply.github.com>
2023-03-19 21:24:29 +0000

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,7 +8,7 @@ public class PlayerController : MonoBehaviour
 {
 
     private Rigidbody2D rb;
-    private float speed = 3f;
+    private float speeder = 3f;
 
     private void Awake()
     {
@@ -17,7 +17,29 @@ public class PlayerController : MonoBehaviour
 
     private void OnMove(InputValue value)
     {
-        rb.velocity = value.Get<Vector2>() * speed;
+        rb.velocity = value.Get<Vector2>() * speeder;
+    }
+
+
+    public float speed = 5.0f; // speed of player movement
+    private Vector3 targetPosition; // position of touch on screen
+
+    // Update is called once per frame
+    void Update()
+    {
+        // check if there is a touch on the screen
+        if (Input.touchCount > 0)
+        {
+            // get the position of the touch on the screen
+            targetPosition = Camera.main.ScreenToWorldPoint(Input.touches[0].position);
+            targetPosition.z = 0; // set z position to 0 for 2D game
+
+            // calculate the direction from current position to target position
+            Vector3 direction = (targetPosition - transform.position).normalized;
+
+            // move the player towards the target position
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+        }
     }
 
 }
