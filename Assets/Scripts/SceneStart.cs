3d8d30a60c4be9e7ff3a6985555dc9b277c2e516
Barengific <75044077+Barengific@users.noreply.github.com>
2023-03-19 22:11:44 +0000

    Update SceneStart.cs

--- Assets/Scripts/SceneStart.cs
+++ Assets/Scripts/SceneStart.cs
@@ -6,8 +6,23 @@ using UnityEngine.SceneManagement;
 public class SceneStart : MonoBehaviour
 {
 
-    public void Scene1()
+    public void ScenePlay()
     {
         SceneManager.LoadScene("PlayScene");
+    }
+
+    public void SceneShop()
+    {
+        SceneManager.LoadScene("ShopScene");
+    }
+
+    public void SceneHome()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+
+    public void SceneSetting()
+    {
+        SceneManager.LoadScene("SettingScene");
     }
 }
