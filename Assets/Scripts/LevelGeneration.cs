8b64cc4022be457ddbfc321d78f3208b0bf80504
Barengific <75044077+Barengific@users.noreply.github.com>
2023-03-20 20:23:33 +0000

    Update LevelGeneration.cs

--- Assets/Scripts/LevelGeneration.cs
+++ Assets/Scripts/LevelGeneration.cs
@@ -13,15 +13,25 @@ public class LevelGeneration : MonoBehaviour
     private void Start()
     {
         GameObject StartPlane1 = Instantiate(StartTile, transform);
+        BoxCollider bc1 = StartPlane1.AddComponent<BoxCollider>();
         StartPlane1.transform.position = new Vector3(0, 7, 0);
+
         GameObject StartPlane2 = Instantiate(StartTile, transform);
         StartPlane2.transform.position = new Vector3(0, -1, 0);
+        BoxCollider bc2 = StartPlane2.AddComponent<BoxCollider>();
+
         GameObject StartPlane3 = Instantiate(StartTile, transform);
         StartPlane3.transform.position = new Vector3(0, -9, 0);
+        BoxCollider bc3 = StartPlane3.AddComponent<BoxCollider>();
+
         GameObject StartPlane4 = Instantiate(StartTile, transform);
         StartPlane4.transform.position = new Vector3(0, -17, 0);
+        BoxCollider bc4 = StartPlane4.AddComponent<BoxCollider>();
+
         GameObject StartPlane5 = Instantiate(StartTile, transform);
         StartPlane5.transform.position = new Vector3(0, -25, 0);
+        BoxCollider bc5 = StartPlane5.AddComponent<BoxCollider>();
+
     }
 
     private void Update()
@@ -35,11 +45,13 @@ public class LevelGeneration : MonoBehaviour
             if (RandomInt1 == 1)
             {
                 GameObject TempTile1 = Instantiate(Tile1, transform);
+                BoxCollider bc1 = TempTile1.AddComponent<BoxCollider>();
                 TempTile1.transform.position = new Vector3(0, -16, 0);
             }
             else if (RandomInt1 == 0)
             {
                 GameObject TempTile1 = Instantiate(Tile2, transform);
+                BoxCollider bc2 = TempTile1.AddComponent<BoxCollider>();
                 TempTile1.transform.position = new Vector3(0, -16, 0);
             }
 
@@ -48,11 +60,13 @@ public class LevelGeneration : MonoBehaviour
             if (RandomInt2 == 1)
             {
                 GameObject TempTile2 = Instantiate(Tile1, transform);
+                BoxCollider bc1 = TempTile2.AddComponent<BoxCollider>();
                 TempTile2.transform.position = new Vector3(0, -24, 0);
             }
             else if (RandomInt2 == 0)
             {
                 GameObject TempTile2 = Instantiate(Tile2, transform);
+                BoxCollider bc2 = TempTile2.AddComponent<BoxCollider>();
                 TempTile2.transform.position = new Vector3(0, -24, 0);
             }
 
