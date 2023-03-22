caae1e0ef636041db5c12bca9802cebf0a736f94
Barengific <75044077+Barengific@users.noreply.github.com>
2023-03-22 22:09:19 +0000

    Update LevelGeneration.cs

--- Assets/Scripts/LevelGeneration.cs
+++ Assets/Scripts/LevelGeneration.cs
@@ -13,24 +13,24 @@ public class LevelGeneration : MonoBehaviour
     private void Start()
     {
         GameObject StartPlane1 = Instantiate(StartTile, transform);
-        StartPlane1.transform.position = new Vector3(0, 7, 0);
+        StartPlane1.transform.position = new Vector2(0, 7);
         StartPlane1.AddComponent<BoxCollider2D>();
 
 
         GameObject StartPlane2 = Instantiate(StartTile, transform);
-        StartPlane2.transform.position = new Vector3(0, -1, 0);
+        StartPlane2.transform.position = new Vector2(0, -1);
         StartPlane2.AddComponent<BoxCollider2D>();
 
         GameObject StartPlane3 = Instantiate(StartTile, transform);
-        StartPlane3.transform.position = new Vector3(0, -9, 0);
+        StartPlane3.transform.position = new Vector2(0, -9);
         StartPlane3.AddComponent<BoxCollider2D>();    
 
         GameObject StartPlane4 = Instantiate(StartTile, transform);
-        StartPlane4.transform.position = new Vector3(0, -17, 0);
+        StartPlane4.transform.position = new Vector2(0, -17);
         StartPlane4.AddComponent<BoxCollider2D>();
 
         GameObject StartPlane5 = Instantiate(StartTile, transform);
-        StartPlane5.transform.position = new Vector3(0, -25, 0);
+        StartPlane5.transform.position = new Vector2(0, -25);
         StartPlane5.AddComponent<BoxCollider2D>();
 
     }
@@ -45,30 +45,34 @@ public class LevelGeneration : MonoBehaviour
 
             if (RandomInt1 == 1)
             {
+                int rndX = Random.Range(-5, 5);
                 GameObject TempTile1 = Instantiate(Tile1, transform);
                 TempTile1.AddComponent<BoxCollider2D>();
-                TempTile1.transform.position = new Vector3(0, -16, 0);
+                TempTile1.transform.position = new Vector2(rndX, -16);
             }
             else if (RandomInt1 == 0)
             {
+                int rndX = Random.Range(-5, 5);
                 GameObject TempTile1 = Instantiate(Tile2, transform);
                 TempTile1.AddComponent<BoxCollider2D>();
-                TempTile1.transform.position = new Vector3(0, -16, 0);
+                TempTile1.transform.position = new Vector2(rndX, -16);
             }
 
             int RandomInt2 = Random.Range(0, 2);
 
             if (RandomInt2 == 1)
             {
+                int rndX = Random.Range(-5, 5);
                 GameObject TempTile2 = Instantiate(Tile1, transform);
                 TempTile2.AddComponent<BoxCollider2D>();
-                TempTile2.transform.position = new Vector3(0, -24, 0);
+                TempTile2.transform.position = new Vector2(rndX, -24);
             }
             else if (RandomInt2 == 0)
             {
+                int rndX = Random.Range(-5, 5);
                 GameObject TempTile2 = Instantiate(Tile2, transform);
                 TempTile2.AddComponent<BoxCollider2D>();
-                TempTile2.transform.position = new Vector3(0, -24, 0);
+                TempTile2.transform.position = new Vector2(rndX, -24);
             }
 
             Index = Index + 15.95f;
