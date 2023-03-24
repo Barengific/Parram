c012bd8ac26ab390815cbd661ff7904ebaee9b08
Barengific <75044077+Barengific@users.noreply.github.com>
2023-03-24 19:59:50 +0000

    Update CollisionDetector.cs

--- Assets/Scripts/CollisionDetector.cs
+++ Assets/Scripts/CollisionDetector.cs
@@ -12,46 +12,45 @@ public class CollisionDetector : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("Collision detected!");
-
-        //point setter
-        if (PlayerPrefs.HasKey("Score"))
+        if (collision.gameObject.tag == "Enemy")
         {
-            int scoreCarried = PlayerPrefs.GetInt("Score");
-            PlayerPrefs.SetInt("Score", int.Parse(scoreText.text) + scoreCarried);
-        }
-        else
-        {
-            Debug.Log("Non Existing Key");
-            PlayerPrefs.SetInt("Score", int.Parse(scoreText.text));
-        }
-
-        //highscore setter
-        if (PlayerPrefs.HasKey("HighScore"))
-        {
-            int currentHighScore = PlayerPrefs.GetInt("HighScore");
-            int newHighScore = int.Parse(scoreText.text);
-
-            if (newHighScore > currentHighScore)
+            //point setter
+            if (PlayerPrefs.HasKey("Score"))
             {
-                PlayerPrefs.SetInt("HighScore", newHighScore);
+                int scoreCarried = PlayerPrefs.GetInt("Score");
+                PlayerPrefs.SetInt("Score", int.Parse(scoreText.text) + scoreCarried);
             }
             else
             {
-                PlayerPrefs.SetInt("HighScore", currentHighScore);
+                Debug.Log("Non Existing Key");
+                PlayerPrefs.SetInt("Score", int.Parse(scoreText.text));
             }
-            
+
+            //highscore setter
+            if (PlayerPrefs.HasKey("HighScore"))
+            {
+                int currentHighScore = PlayerPrefs.GetInt("HighScore");
+                int newHighScore = int.Parse(scoreText.text);
+
+                if (newHighScore > currentHighScore)
+                {
+                    PlayerPrefs.SetInt("HighScore", newHighScore);
+                }
+                else
+                {
+                    PlayerPrefs.SetInt("HighScore", currentHighScore);
+                }
+
+            }
+            else
+            {
+                PlayerPrefs.SetInt("HighScore", int.Parse(scoreText.text));
+            }
+
+            Destroy(collision.gameObject);
+            SceneManager.LoadScene("MenuScene");
         }
-        else
-        {
-            PlayerPrefs.SetInt("HighScore", int.Parse(scoreText.text));
-        }
 
-
-
-
-        Destroy(collision.gameObject);
-        SceneManager.LoadScene("MenuScene");
 
     }
 }
